03cd42d9d49415cf55cffdf264177d14e7e338fa
Kristina Bilych <144391663+bilychk@users.noreply.github.com>
2026-08-04 10:03:18 +0300

    Add TimeConverter class to convert hours to minutes

--- Basics/Main.java
+++ Basics/Main.java
@@ -133,7 +133,7 @@ class Temperature {
 }
 
 
-
+//6
 class NumbersAverage
 {
     public static void main(String[] args) {
@@ -151,6 +151,27 @@ class NumbersAverage
         double average = (firstNumber + secondNumber + thirdNumber) / 3;
 
         System.out.println("The average of three numbers is: " + average);
+
+        scanner.close();
+    }
+}
+
+
+//7
+class TimeConverter
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter hours: ");
+        int hours = scanner.nextInt();
+
+        System.out.print("Enter minutes: ");
+        int minutes = scanner.nextInt();
+
+        int total = hours * 60 + minutes;
+
+        System.out.println("Total minutes: " + total);
 
         scanner.close();
     }
