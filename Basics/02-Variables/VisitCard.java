723e54aa4ad52af4a751289c62ac7a883ebec890
Kristina Bilych <144391663+bilychk@users.noreply.github.com>
2026-08-04 10:45:23 +0300

    Add VisitCard class with personal information

---
+++ Basics/02-Variables/VisitCard.java
@@ -0,0 +1,11 @@
+public class VisitCard
+{
+    public static void main(String[] args) {
+        String name = "Kristina";
+        int age = 21;
+        String city = "Tallinn";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
