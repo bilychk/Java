b5cc7e5dc4f53349b4d1a8586e3d0c9e50d25b63
Kristina Bilych <144391663+bilychk@users.noreply.github.com>
2026-08-04 10:01:51 +0300

    Add NumbersAverage class to calculate average

--- Basics/Main.java
+++ Basics/Main.java
@@ -131,3 +131,27 @@ class Temperature {
 
     }
 }
+
+
+
+class NumbersAverage
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double firstNumber = scanner.nextDouble(); //Srednee ne vsegda celoe 4islo, poetomu double
+
+        System.out.print("Enter the second number: ");
+        double secondNumber = scanner.nextDouble();
+
+        System.out.print("Enter the third number: ");
+        double thirdNumber = scanner.nextDouble();
+
+        double average = (firstNumber + secondNumber + thirdNumber) / 3;
+
+        System.out.println("The average of three numbers is: " + average);
+
+        scanner.close();
+    }
+}
