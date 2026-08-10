92616b1db4d85a7f9d63e31a8265f6ca632b8fb9
bilychk <bilychkristina@gmail.com>
2026-08-10 18:25:43 +0300

    code edit

--- Basics/07-Loops/Loops.java
+++ Basics/07-Loops/Loops.java
@@ -6,13 +6,20 @@ public class Loops {
         // while
         // do while
         // for
+        // for each
 
-        int i = 1;
-        while(i <= 5){
-            System.out.print(i);
-            i += 1;
-            i++; // i +=1 Works only for one!
+        // int i = 1;
+        // while(i <= 5){
+        //     System.out.print(i);
+        //     i += 1;
+        //     i++; // i +=1 Works only for one!
 
+        // for i in lst -) []
+        // for i in wange(start,end,step)
+        int sum = 0;
+        for(int i = 1; i <= 10; i++){  // i exist only inside this for loop.
+            sum += i;
+            System.out.println("i = " + i + ", sum = " + sum);
+        }
         }
     }
-}
