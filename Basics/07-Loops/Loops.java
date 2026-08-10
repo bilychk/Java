d68936e768e8f0ac6a7e81ca4333d508e239643b
bilychk <bilychkristina@gmail.com>
2026-08-10 18:16:28 +0300

    New class

---
+++ Basics/07-Loops/Loops.java
@@ -0,0 +1,18 @@
+
+public class Loops {
+    
+    public static void main(String[] args)
+    {
+        // while
+        // do while
+        // for
+
+        int i = 1;
+        while(i <= 5){
+            System.out.print(i);
+            i += 1;
+            i++; // i +=1 Works only for one!
+
+        }
+    }
+}
