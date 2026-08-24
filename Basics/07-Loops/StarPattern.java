c881746935d1cf6c6b216c9a58c7f69ea13dcea9
bilychk <bilychkristina@gmail.com>
2026-08-24 16:13:14 +0300

    comment add

--- Basics/07-Loops/StarPattern.java
+++ Basics/07-Loops/StarPattern.java
@@ -1,12 +1,11 @@
 public class StarPattern {
     public static void main(String[] args) {
 
-        for (int row = 1; row <= 7; row++) {  // rows
-            for (int column = 1; column <= 7; column++) {  //columns
+        for (int row = 1; row <= 7; row++) { // создаем 7 строк
 
-                if (column == 4
-                        || column == row // or
-                        || column == 8 - row) {
+            for (int column = 1; column <= 7; column++) { // создаем 7 позиций
+
+                if (row == column || row + column == 8 || column == 4) { // ставить звездочку или оставить пустое место
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
