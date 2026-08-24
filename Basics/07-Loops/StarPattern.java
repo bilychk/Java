7eec0659aa4b2d71049cefc833230f144e59afdb
Kristina Bilych <144391663+bilychk@users.noreply.github.com>
2026-08-24 09:33:51 +0300

    Improve loop structure in StarPattern.java
    Refactor nested loops for better readability.

---
+++ Basics/07-Loops/StarPattern.java
@@ -0,0 +1,19 @@
+public class Figure {
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 7; row++) {  // rows
+            for (int column = 1; column <= 7; column++) {  //columns
+
+                if (column == 4
+                        || column == row // or
+                        || column == 8 - row) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
